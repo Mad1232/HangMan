d34cda189da6e41a997419467c01123d5ade3f17
Prakarsha <46485511+Mad1232@users.noreply.github.com>
2023-12-20 18:47:13 +0545

    Update player.java

--- player.java
+++ player.java
@@ -1,5 +1,44 @@
 package hangman;
 
 public class player {
+    private String name;
+    private int score;
 
+    // Constructors
+    public player(String name) {
+        this.name = name;
+        this.score = 0;
+    }
+
+    // Getter and setter methods
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void incrementScore() {
+        score++;
+    }
+
+    // Increment the score based on the number of attempts
+    public void updateScore(int attempts) {
+        if (attempts == 1) {
+            score += 100;
+        } else if (attempts == 2) {
+            score += 60;
+        } else if (attempts == 3) {
+            score += 40;
+        } else if (attempts == 4) {
+            score += 20;
+        } else {
+            score += 10;
+        }
+    }
 }
